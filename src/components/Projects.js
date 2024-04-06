27dcff6cca719d6d02fde4b22e26bfd7de525173
rawanAlzubaidi <101676826+rawanAlzubaidi@users.noreply.github.com>
2024-04-06 14:03:29 -0500

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Modal } from "react-bootstrap";
 import tamumsa from "../assets/img/TamuMSA.png";
-import csce315 from "../assets/img/cscs315.png";
-import howdyHack from "../assets/img/HowdyHack.png";
 import blogimage from "../assets/img/blogkitabat.png";
 import photoimage from "../assets/img/photo.png";
 import lingoimage from "../assets/img/lingoleap.png";
@@ -38,7 +36,7 @@ export const Projects = () => {
             description: "Project description here.",
             headers: ['Inspiration/Problem', 'Solution', 'Technologies/Tools'],
             paragraphs: [
-                'Paragraph for Inspiration/Problem. This is an example text.',
+                'We were tasked with proposing three ideas for our capstone project. I came up with this idea past midnight when this was due. I tried to avoid jumping to the solution right away as we software developers we tend to do that. I tried to think of problems people I care about might have. There is nothing I adore more than my niece and nephews. So here it was, I can create something for them. The oldest is 7 and the youngest is 2 years old, You might wonder what kind of problems they could possibly encounter. Two are learning Finish and Arabic, and 3 are learning english and Arabic. So I thought I can create a language learning app for kids. Although this project still requires significant work to make a meaningful impact on young language learners, I am proud of what we have achieved during the six-week development cycle.',
                 'Our young users are prompted to explore and interact with the app to find and snap pictures of each vocabulary in the selected set. This interaction fosters a real-world connection as they visually identify these words, reinforcing their vocabulary comprehension. As they successfully collect pictures of all the items in a word set, they unlock badges within the app, adding an element of excitement and motivation to their learning journey. They are then challenged to recall the vocabulary with quizzes, which reinforces their word retention and makes learning a dynamic and memorable process.',
                 'In the development of our application, we have employed a suite of advanced technologies for both the front-end and back-end, primarily centered around UIKit and Swift. Swift is instrumental in creating a seamless and responsive user interface across all Apple platforms. For object detection and language processing, we integrate a YOLOv8 model, Google Cloud Vision, and OpenAI, with each playing a pivotal role in enhancing the app capabilities. The YOLOv8 model is essential for accurate real-time object detection, while Google Cloud Vision provides advanced image analysis. OpenAI adds a layer of sophisticated language interpretation and processing. Additionally, we incorporate Google Translate and Google Text-to-Speech to further enrich the user experience. Google Translate is crucial in providing accurate and instantaneous language translations, vital for the learning process in our app. Google Text-to-Speech enhances the interactive element of the app, enabling it to communicate with users in a more engaging and accessible way. Data management and user authentication are handled through Firebase Cloud Storage and Firebase Authentication, ensuring secure and efficient storage and user management.'
             ],
@@ -58,13 +56,12 @@ export const Projects = () => {
         },
         photo: {
             img: photoimage,
-            title: "Capstone project",
+            title: "photography portfolio",
             description: "Project description here.",
             headers: ['Inspiration/Problem', 'Solution', 'Technologies/Tools'],
             paragraphs: [
                 'I like being able to blend my different interest, photography, animation, and  programing.  After graduating I wanted to take a break and create something simple for fun.',
                 'The website is built using the React framework, chosen for its flexibility and efficiency in developing interactive user interfaces. The images are stored and fetched dynamically from Firebase, ensuring that the portfolio is always up-to-date with my latest work.',
-                'Paragraph for Technologies/Tools. Yet another example text.'
             ],
             technologies: ['html5', 'css3', 'javascript', 'bootstrap'],
             link: { url: "https://example.com" }
@@ -107,7 +104,7 @@ export const Projects = () => {
                 </Row>
             </Container>
             {selectedProject && (
-                <Modal show={showModal} onHide={() => setShowModal(false)} centered size="lg">
+                <Modal show={showModal} onHide={() => setShowModal(false)} centered size="xl">
                     <Modal.Header closeButton>
                         <Modal.Title>{selectedProject.title}</Modal.Title>
                     </Modal.Header>
